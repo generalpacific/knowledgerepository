0f2e472d0a9c470509f62b7a8de8284a9c67e3fe
generalpacific <prash.c.29@gmail.com>
2023-05-13 22:42:04 -0700

    Send todays date in the art of the day page instead of hard coding

--- web/src/pages/artoftheday.js
+++ web/src/pages/artoftheday.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 function ArtOfTheDay(props) {
+  // generate today's date in the format 'YYYY-MM-DD'
+  // By defaul the timezone is the browsers timezone.
+  const today = new Date().toISOString().slice(0, 10);
+  const url = `https://6h5c17qwla.execute-api.us-east-2.amazonaws.com/prod/artoftheday?date=${today}`;
   return (
     <div>
-      <img src='https://6h5c17qwla.execute-api.us-east-2.amazonaws.com/prod/artoftheday?date=2023-05-14' alt="API Image" />
+      <img src={url} alt="Art of the Day" />
     </div>
   );
 }
