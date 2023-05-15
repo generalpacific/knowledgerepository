afa9a197f7d3378434d90ed13b2cf62130081e24
generalpacific <prash.c.29@gmail.com>
2023-05-14 21:29:13 -0700

    Fix date calculation to get the correct timezone date

--- web/src/pages/artoftheday.js
+++ web/src/pages/artoftheday.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 function ArtOfTheDay(props) {
   // generate today's date in the format 'YYYY-MM-DD'
-  // By defaul the timezone is the browsers timezone.
-  const today = new Date().toISOString().slice(0, 10);
+  const tzOffset = new Date().getTimezoneOffset() * 60000; // Timezone offset in milliseconds
+  const today = new Date(Date.now() - tzOffset).toISOString().slice(0, 10);
+  console.error("Calling artoftheday for: " + today)
   const url = `https://6h5c17qwla.execute-api.us-east-2.amazonaws.com/prod/artoftheday?date=${today}`;
   return (
     <div>
