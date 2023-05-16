1f512b2a26b05ce2939d5aad11a13927c614f2e9
generalpacific <prash.c.29@gmail.com>
2023-05-15 22:35:53 -0700

    Display the prompt that was used to generate the art

--- web/src/pages/artoftheday.js
+++ web/src/pages/artoftheday.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function ArtOfTheDay(props) {
-  // generate today's date in the format 'YYYY-MM-DD'
-  const tzOffset = new Date().getTimezoneOffset() * 60000; // Timezone offset in milliseconds
-  const today = new Date(Date.now() - tzOffset).toISOString().slice(0, 10);
-  console.error("Calling artoftheday for: " + today)
-  const url = `https://6h5c17qwla.execute-api.us-east-2.amazonaws.com/prod/artoftheday?date=${today}`;
+  const [imageData, setImageData] = useState(null);
+  const [promptData, setPromptData] = useState(null);
+
+  useEffect(() => {
+    const tzOffset = new Date().getTimezoneOffset() * 60000;
+    const today = new Date(Date.now() - tzOffset).toISOString().slice(0, 10);
+    const url = `https://6h5c17qwla.execute-api.us-east-2.amazonaws.com/prod/artoftheday?date=${today}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setImageData(data.image);
+        setPromptData(data.prompt);
+      })
+      .catch(error => {
+        console.log('Error fetching image data:', error);
+      });
+  }, []);
+
   return (
     <div>
-      <img src={url} alt="Art of the Day" />
+      {promptData && <h style={{ marginBottom: '10px' }}>{promptData}</h>}
+      <br />
+      <br />
+      {imageData && <img src={`data:image/png;base64,${imageData}`} alt="Art of the Day" />}
     </div>
   );
 }
