40f6acbc8a4c460efca7cc13c7008b57804c3df9
generalpacific <prash.c.29@gmail.com>
2023-05-22 18:14:16 -0700

    Add initial version of knowledge query api

---
+++ knowledge_query_api_lambda/lambda_handler.py
@@ -0,0 +1,102 @@
+import boto3
+import datetime
+import dateutil.tz
+import json
+import os
+import random
+from botocore.exceptions import ClientError
+
+
+def __get_entities(dynamodb, source, title):
+    if source == 'KINDLE':
+        table = os.environ['KINDLE_HIGHLIGHTS_TABLE']
+    elif source == 'NOTION':
+        table = os.environ['NOTION_BOOK_QUOTES_TABLE']
+    else:
+        raise ClientError("Illegal source", source)
+
+    return_highlights = set()
+    try:
+        # Set up the Query parameters
+        query_params = {
+            'TableName': table,
+            'IndexName': 'tite-index',
+            'KeyConditionExpression': 'tite = :title',
+            'ExpressionAttributeValues': {
+                ':title': {'S': title}
+            }
+        }
+
+        # Query the table using the GSI on the title column
+        response = dyndb.query(**query_params)
+    except ClientError as e:
+        print("ERROR while getting latest quote. Returning empty. Error: " +
+              e.response['Error']['Message'])
+        return return_highlights
+    else:
+        if 'Items' in response:
+            items = response['Items']
+            for item in items:
+                return_highlights.add(item['highlight']['S'])
+        return return_highlights
+
+
+def lambda_handler(event, context):
+    if 'queryStringParameters' not in event:
+        print("No queryStringParameters in event")
+        return {
+            'statusCode': '400',
+            'body': "No queryStringParameters in event",
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+            }
+        }
+
+    if event['queryStringParameters'] is None:
+        print("queryStringParameters in event is none")
+        return {
+            'statusCode': '400',
+            'body': "queryStringParameters in event is none",
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+            }
+        }
+
+    query_string_parameters = event['queryStringParameters']
+
+    if 'source' not in query_string_parameters:
+        print("No source in event[queryStringParameters]")
+        return {
+            'statusCode': '400',
+            'body': "No source in event[queryStringParameters]. Provide source for which you need entities for.",
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+            }
+        }
+    source = query_string_parameters['source']
+
+    if 'title' not in query_string_parameters:
+        print("No title in event[queryStringParameters]")
+        return {
+            'statusCode': '400',
+            'body': "No title in event[queryStringParameters]. Provide title for which you need entities for.",
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+            }
+        }
+    title = query_string_parameters['title']
+
+    dynamodb = boto3.client('dynamodb')
+
+    entities = __get_entities(dynamodb, source, title)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(entities),
+        'headers': {
+            "Content-Type": "application/json",
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        }
+    }
---
+++ knowledge_query_api_lambda/test_lambda_handler.py
@@ -0,0 +1,161 @@
+import datetime
+import json
+import os
+import unittest
+from unittest.mock import patch, MagicMock
+
+import boto3
+import dateutil.tz
+import lambda_handler
+import mock
+import moto
+import pytest
+
+NOTION_TABLE_NAME = "notion-book-quotes"
+KINDLE_HIGHLIGHTS_TABLE = "kindle-highlights-table"
+DAILY_DIGEST_TABLE = "daily-digest-table"
+ANKIENTITIES_TABLE = "ankientities-table"
+
+DAILY_DIGEST_ID_TEST_DATA = {"TWITTER": ["1", "2"],
+                             "KINDLE": ["3", "4"], "NOTION": ["5", "6"]
+                             }
+
+EXPECTED_DAILY_DIGEST_TEST_DATA = {"digest": [
+    {"entityid": "5", "title": "title5", "author": "author5", "quote": "quote5", "plusones": "12"},
+    {"entityid": "4", "title": "title4", "author": "author4", "highlight": "highlight4", "plusones": "12"},
+    {"entityid": "1", "tweet_id": "tweetid1", "plusones": "12"},
+    {"entityid": "6", "title": "title6", "author": "author6", "quote": "quote6", "plusones": "12"},
+    {"entityid": "2", "tweet_id": "tweetid2", "plusones": "12"},
+    {"entityid": "3", "title": "title3", "author": "author3", "highlight": "highlight3", "plusones": "12"}]}
+
+
+@pytest.fixture
+def lambda_environment():
+    print("Setting lambda environment variables")
+    os.environ['NOTION_BOOK_QUOTES_TABLE'] = NOTION_TABLE_NAME
+    os.environ['KINDLE_HIGHLIGHTS_TABLE'] = KINDLE_HIGHLIGHTS_TABLE
+    os.environ['DAILY_DIGEST_TABLE'] = DAILY_DIGEST_TABLE
+    os.environ['ANKIENTITIES_TABLE'] = ANKIENTITIES_TABLE
+
+
+@pytest.fixture
+def create_dynamodb_tables():
+    with moto.mock_dynamodb():
+        client = boto3.client("dynamodb")
+        client.create_table(
+            AttributeDefinitions=[
+                {"AttributeName": "date", "AttributeType": "S"}
+            ],
+            TableName=DAILY_DIGEST_TABLE,
+            KeySchema=[
+                {"AttributeName": "date", "KeyType": "HASH"},
+            ],
+            BillingMode="PAY_PER_REQUEST"
+        )
+
+        client.create_table(
+            AttributeDefinitions=[
+                {"AttributeName": "entityid", "AttributeType": "S"}
+            ],
+            TableName=ANKIENTITIES_TABLE,
+            KeySchema=[
+                {"AttributeName": "entityid", "KeyType": "HASH"}
+            ],
+            BillingMode="PAY_PER_REQUEST"
+        )
+
+        client.create_table(
+            AttributeDefinitions=[
+                {"AttributeName": "id", "AttributeType": "S"}
+            ],
+            TableName=NOTION_TABLE_NAME,
+            KeySchema=[
+                {"AttributeName": "id", "KeyType": "HASH"}
+            ],
+            BillingMode="PAY_PER_REQUEST"
+        )
+
+        client.create_table(
+            AttributeDefinitions=[
+                {"AttributeName": "id", "AttributeType": "S"}
+            ],
+            TableName=KINDLE_HIGHLIGHTS_TABLE,
+            KeySchema=[
+                {"AttributeName": "id", "KeyType": "HASH"}
+            ],
+            BillingMode="PAY_PER_REQUEST"
+        )
+
+        yield DAILY_DIGEST_TABLE, ANKIENTITIES_TABLE
+
+
+@pytest.fixture
+def populate_dynamodb_table_with_data(create_dynamodb_tables):
+    """Creates data for daily digest"""
+
+    table = boto3.resource("dynamodb").Table(DAILY_DIGEST_TABLE)
+    pacific_tz = dateutil.tz.gettz('US/Pacific')
+    today_date = datetime.datetime.now(tz=pacific_tz).strftime("%Y-%m-%d")
+    print("Adding digest for date: ", today_date)
+    txs = [
+        {"date": today_date,
+         "digest": json.dumps(DAILY_DIGEST_ID_TEST_DATA)},
+    ]
+    for tx in txs:
+        table.put_item(Item=tx)
+
+    table = boto3.resource("dynamodb").Table(ANKIENTITIES_TABLE)
+    print("Adding data to anki entities table")
+    txs = [
+        {"entityid": "1", "foreign_id": "tweetid1", "source": "TWITTER", "plus_one": "12"},
+        {"entityid": "2", "foreign_id": "tweetid2", "source": "TWITTER", "plus_one": "12"},
+        {"entityid": "3", "foreign_id": "kindle3", "source": "KINDLE", "plus_one": "12"},
+        {"entityid": "4", "foreign_id": "kindle4", "source": "KINDLE", "plus_one": "12"},
+        {"entityid": "5", "foreign_id": "notion5", "source": "NOTION", "plus_one": "12"},
+        {"entityid": "6", "foreign_id": "notion6", "source": "NOTION", "plus_one": "12"},
+    ]
+    for tx in txs:
+        table.put_item(Item=tx)
+
+    table = boto3.resource("dynamodb").Table(NOTION_TABLE_NAME)
+    print("Adding data to notion table")
+    txs = [
+        {"id": "notion5", "author": "author5", "quote": "quote5", "tite": "title5"},
+        {"id": "notion6", "author": "author6", "quote": "quote6", "tite": "title6"},
+    ]
+    for tx in txs:
+        table.put_item(Item=tx)
+
+    table = boto3.resource("dynamodb").Table(KINDLE_HIGHLIGHTS_TABLE)
+    print("Adding data to kindle table")
+    txs = [
+        {"id": "kindle3", "author": "author3", "highlight": "highlight3", "tite": "title3"},
+        {"id": "kindle4", "author": "author4", "highlight": "highlight4", "tite": "title4"},
+    ]
+    for tx in txs:
+        table.put_item(Item=tx)
+
+
+## Tests start here.
+
+def ordered(obj):
+    if isinstance(obj, dict):
+        return sorted((k, ordered(v)) for k, v in obj.items())
+    if isinstance(obj, list):
+        return sorted(ordered(x) for x in obj)
+    else:
+        return obj
+
+
+def test_lambda_daily_digest(lambda_environment, populate_dynamodb_table_with_data):
+    """Tests the lambda function for getting the daily digest."""
+
+    response = lambda_handler.lambda_handler({}, {})
+
+    print("response: ", response)
+    assert response["statusCode"] == 200
+
+    digest = json.loads(response["body"])["digest"]
+    expected_digest = EXPECTED_DAILY_DIGEST_TEST_DATA['digest']
+    assert len(digest) == len(expected_digest)
+    assert ordered(digest) == ordered(expected_digest)
