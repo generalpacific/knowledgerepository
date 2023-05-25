988274be546cf55b8ef9c9bdd04d6ec4bac30a19
generalpacific <prash.c.29@gmail.com>
2023-05-24 18:32:58 -0700

    Apply prettier on files in src

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route}
-    from 'react-router-dom';
-import Home from './pages';
-import KnowledgeDailyDigest from './pages/knowledgedailydigest';
-import ArtOfTheDay from './pages/artoftheday';
-import KnowledgeQuery from './pages/knowledgequery';
-  
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages";
+import KnowledgeDailyDigest from "./pages/knowledgedailydigest";
+import ArtOfTheDay from "./pages/artoftheday";
+import KnowledgeQuery from "./pages/knowledgequery";
+
 function App() {
-return (
+  return (
     <Router>
-    <Navbar />
-    <Routes>
-        <Route exact path='/' exact element={<Home />} />
-        <Route path='/knowledgedailydigest' element={<KnowledgeDailyDigest />} />
-        <Route path='/artoftheday' element={<ArtOfTheDay />} />
-        <Route path='/knowledgequery' element={<KnowledgeQuery />} />
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" exact element={<Home />} />
+        <Route
+          path="/knowledgedailydigest"
+          element={<KnowledgeDailyDigest />}
+        />
+        <Route path="/artoftheday" element={<ArtOfTheDay />} />
+        <Route path="/knowledgequery" element={<KnowledgeQuery />} />
+      </Routes>
     </Router>
-);
+  );
 }
-  
+
 export default App;
