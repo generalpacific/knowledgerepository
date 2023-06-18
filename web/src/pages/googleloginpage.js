a3fdf5b3da8e1d586dd0d7556373d7594da0712f
generalpacific <prash.c.29@gmail.com>
2023-06-17 18:46:20 -0700

    Wrap in GoogleOAuthProvider

--- web/src/pages/googleloginpage.js
+++ web/src/pages/googleloginpage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { GoogleLogin } from '@react-oauth/google';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const GoogleLoginButton = () => {
   const responseGoogle = (response) => {
@@ -17,12 +18,13 @@ const GoogleLoginButton = () => {
       <h2>React Google Login</h2>
       <br />
       <br />
-      <GoogleLogin
-        clientId="481783681600-g219mmrl19blv252h8b6t10981rqi8bq.apps.googleusercontent.com"
-        buttonText="Sign in with Google"
-        onSuccess={responseGoogle}
-        onFailure={onFailure}
-      />
+      <GoogleOAuthProvider clientId="481783681600-g219mmrl19blv252h8b6t10981rqi8bq.apps.googleusercontent.com">
+        <GoogleLogin
+          buttonText="Sign in with Google"
+          onSuccess={responseGoogle}
+          onFailure={onFailure}
+        />
+      </GoogleOAuthProvider>
     </div>
   );
 };
