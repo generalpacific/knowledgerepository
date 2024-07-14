010ff4300f77cb4dc2f48c4b44a7cc3d63e4c035
generalpacific <prash.c.29@gmail.com>
2024-07-13 22:02:49 -0700

    [ChromeExtension] add ability to ingest entity by api call

--- web_entity_ingestore_chrome_extension/popup.js
+++ web_entity_ingestore_chrome_extension/popup.js
@@ -6,6 +6,26 @@ const setDOMInfo = info => {
   document.getElementById('entity_source').textContent = info.pageUrl;
 };
 
+
+const sendToServer = (data) => {
+  fetch('https://abc.com/ingest', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+    alert('Ingested successfully!');
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+    alert('Failed to ingest. Error: ' + error);
+  });
+};
+
 // Once the DOM is ready...
 window.addEventListener('DOMContentLoaded', () => {
   // ...query for the active tab...
@@ -21,5 +41,19 @@ window.addEventListener('DOMContentLoaded', () => {
         // ...also specifying a callback to be called 
         //    from the receiving end (content script).
         setDOMInfo);
+  });
+
+  document.getElementById('ingestButton').addEventListener('click', () => {
+    const highlightedEntity = document.getElementById('highlighted_entity').textContent;
+    const entityTitle = document.getElementById('entity_title').textContent;
+    const entitySource = document.getElementById('entity_source').textContent;
+
+    const data = {
+      highlightedEntity: highlightedEntity,
+      entityTitle: entityTitle,
+      entitySource: entitySource
+    };
+
+    sendToServer(data);
   });
 });
